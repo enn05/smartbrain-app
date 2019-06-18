4b1c5dbc36173124b96aab5687a93288e587b8ac
kenneth hunat <niineetz05@gmailcom>
2019-06-19 01:16:19 +0800

    Update Components

--- src/components/Navigation/Navigation.js
+++ src/components/Navigation/Navigation.js
@@ -1,23 +1,32 @@
 import React from 'react';
 
-const Navigation = () => {
+const Navigation = ({ onRouteChange, isSignedIn }) => {
+    // if (isSignedIn) {
+    //     return (
+    //         <nav style={{display: 'flex', justifyContent: 'flex-end'}}>
+    //             <p onClick={() => onRouteChange('signout')} className='f3 link dim black underline pa3 pointer'>Sign Out</p>
+    //         </nav>
+    //     )
+    // } else {
+    //     return (
+    //         <nav style={{display: 'flex', justifyContent: 'flex-end'}}>
+    //             <p onClick={() => onRouteChange('signin')} className='f3 link dim black underline pa3 pointer'>Sign In</p>
+    //             <p onClick={() => onRouteChange('Register')} className='f3 link dim black underline pa3 pointer'>Register</p>
+    //         </nav>
+    //     )
+    // }
     return (
+        isSignedIn
+        ?
         <nav style={{display: 'flex', justifyContent: 'flex-end'}}>
-            <p className='f3 link dim black underline pa3 pointer'>Sign Out</p>
+            <p onClick={() => onRouteChange('signout')} className='f3 link dim black underline pa3 pointer'>Sign Out</p>
         </nav>
-        // <nav class="db dt-l w-100 border-box pa3 ph5-l">
-        //     <a class="db dtc-l v-mid mid-gray link dim w-100 w-25-l tc tl-l mb2 mb0-l" href="#" title="Home">
-        //         <img src="http://tachyons.io/img/logo.jpg" class="dib w2 h2 br-100" alt="Site Name" />
-        //     </a>
-        //         <div class="db dtc-l v-mid w-100 w-75-l tc tr-l">
-        //             <a class="link dim dark-gray f6 f5-l dib mr3 mr4-l" href="#" title="Home">Home</a>
-        //             <a class="link dim dark-gray f6 f5-l dib mr3 mr4-l" href="#" title="How it Works">How it Works</a>
-        //             <a class="link dim dark-gray f6 f5-l dib mr3 mr4-l" href="#" title="Blog">Blog</a>
-        //             <a class="link dim dark-gray f6 f5-l dib mr3 mr4-l" href="#" title="Press">Press</a>
-        //             <a class="link dim dark-gray f6 f5-l dib" href="#" title="Contact">Contact</a>
-        //         </div>
-        // </nav>
-    )
+        :
+        <nav style={{display: 'flex', justifyContent: 'flex-end'}}>
+            <p onClick={() => onRouteChange('signin')} className='f3 link dim black underline pa3 pointer'>Sign In</p>
+            <p onClick={() => onRouteChange('Register')} className='f3 link dim black underline pa3 pointer'>Register</p>
+        </nav>
+    ); 
 }
         
 export default Navigation;
