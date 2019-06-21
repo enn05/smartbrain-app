0596240c133deb0f4c61d264c85685fa32d79a6c
kenneth hunat <niineetz05@gmailcom>
2019-06-21 18:14:37 +0800

    Edit app.js

--- src/components/Signin/Signin.js
+++ src/components/Signin/Signin.js
@@ -28,9 +28,6 @@ class Signin extends React.Component {
         })
             .then(response => response.json())
             .then(data => {
-                // if (data === 'success') {
-                //     this.props.onRouteChange('home');
-                // }
                 if(data.id){
                     this.props.loadUser(data);
                     this.props.onRouteChange('home');
